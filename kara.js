8cc59461b5900d406d3a2a1ca6007cd17e8583fa
Tom Hofmann <tom@scheidweg.net>
2024-06-21 14:28:24 +0200

    Better defaults, better jsdoc

--- kara.js
+++ kara.js
@@ -12,6 +12,13 @@ class InvalidMoveException extends Error {
   }
 }
 
+class TooManyMovesException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
 const GRID_SIZE = [8, 8];
 
 /** 2D grid cell coordinates. */
@@ -19,20 +26,20 @@ class Coordinates {
   static fromXy(x, y) {
     return new Coordinates(x, y);
   }
-  
+
   static fromCopy(coords) {
     return new Coordinates(coords.x, coords.y);
   }
-  
+
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
-  
+
   add(coords) {
     return Coordinates.fromXy(this.x + coords.x, this.y + coords.y);
   }
-  
+
   move(direction) {
     return direction.apply(this);
   }
@@ -60,8 +67,8 @@ class Direction {
 function createDirections() {
   let up = new Direction("↑", 0, -1, 0);
   let down = new Direction("↓", 0, 1, Math.PI);
-  let right = new Direction("→", 1, 0, Math.PI/2);
-  let left = new Direction("←", -1, 0, -Math.PI/2);
+  let right = new Direction("→", 1, 0, Math.PI / 2);
+  let left = new Direction("←", -1, 0, -Math.PI / 2);
   up.left = left;
   up.right = right;
   left.left = down;
@@ -78,12 +85,12 @@ function createDirections() {
   })
 }
 
+/** All known directions. */
 const Directions = createDirections();
-
 
 class Cell {
   static fromString(spec) {
-    switch(spec) {
+    switch (spec) {
       case " ":
       case "_":
         return Cells.EMPTY;
@@ -103,20 +110,21 @@ class Cell {
         return new Cell(spec, spec);
     }
   }
-  constructor(display="?", type=EMPTY) {
-    this.display= display;
+  constructor(display = "?", type = EMPTY) {
+    this.display = display;
     this.type = type;
   }
-  
+
   draw(x, y) {
     text(this.display, x, y);
   }
-  
+
   toString() {
     return this.type;
   }
 }
 
+/** Enumeration of all Cell types. */
 const Cells = Object.freeze({
   EMPTY: new Cell(" ", "_"),
   TREE: new Cell("🌳", "T"),
@@ -142,9 +150,9 @@ class Grid {
   static copy(grid) {
     return Grid.fromStringSpec(grid.toString());
   }
-  
+
   /** Creates a new grid from a single, multi-line text spec.
-      Lines are trimmed, so they must start with a non-whitespace
+      Lines are trimmed, so they must start and end with a non-whitespace
       cell (or '_'). */
   static fromStringSpec(spec) {
     let lines = spec.split(/\r?\n/g);
@@ -153,13 +161,13 @@ class Grid {
     }
     return Grid.fromStringArray(lines);
   }
-  
+
   /** Creates a new grid from a list of lines, top to bottom.
       Each line must have the same number of characters as the first.
       Lines are not trimmed. */
   static fromStringArray(lines) {
     // Use iterator based string decomposition to handle all code points.
-    let width = [...lines[0]].length; 
+    let width = [...lines[0]].length;
     let height = lines.length;
     let grid = new Grid([width, height]);
     let y = 0
@@ -177,7 +185,7 @@ class Grid {
     }
     return grid;
   }
-  
+
   constructor(size = GRID_SIZE) {
     assert(size[0] >= 1 && size[1] >= 1, `unexpected grid size: ${size}`);
     this.size = [...size]; // defensive clone
@@ -188,7 +196,10 @@ class Grid {
     }
   }
 
-  /** Finds the first Kara-cell and returns a corresponding Kara. Removes all Kara-like cells. */
+  /** Finds the first Kara-cell and returns a corresponding Kara. Removes all
+    * Kara-like cells. If no Kara-cell exists, the first empty cell is used for
+    * Kara, with a direction of RIGHT. If no empty cells exist, undefined is
+    * returned. */
   findKara() {
     let kara = undefined;
     let firstEmpty = undefined;
@@ -233,12 +244,12 @@ class Grid {
     }
     return kara;
   }
-  
+
   /** Returns the cell at the given coordinates. */
   at(coords) {
     return this.grid[coords.y][coords.x];
   }
-  
+
   /** Sets the cell at the given coordinates. */
   set(coords, cell) {
     this.grid[coords.y][coords.x] = cell;
@@ -248,24 +259,24 @@ class Grid {
   clear(coords) {
     this.grid[coords.y][coords.x] = Cells.EMPTY;
   }
-  
+
   /** Draws the grid using processing primitives, from the given 
       upper-left corner and using the given cell size, both in pixels. */
-  draw(cell_size=25) {
+  draw(cell_size = 25) {
     push();
     stroke('rgba(115,115,115,0.5)');
     textAlign(CENTER, CENTER);
-    
+
     for (let y = 0; y < this.grid.length; y++) {
       let line = this.grid[y];
       for (let x = 0; x < line.length; x++) {
         let cell = this.at(Coordinates.fromXy(x, y));
         push();
-        translate(x * cell_size, y* cell_size);
+        translate(x * cell_size, y * cell_size);
         square(0, 0, cell_size);
         // Processing coords grow from upper-left to lower-right.
         // We use textAlign(CENTER, CENTER), so specify midpoint.
-        translate(0.5*cell_size, 0.5*cell_size);
+        translate(0.5 * cell_size, 0.5 * cell_size);
         cell.draw(0, 0);
         pop();
       }
@@ -276,7 +287,7 @@ class Grid {
   gridSize() {
     return Coordinates.fromXy(this.grid[0].length, this.grid.length);
   }
-    
+
   toString() {
     return this.grid.map(line => line.map(cell => cell.toString()).join("")).join("\n");
   }
@@ -285,14 +296,14 @@ class Grid {
 
 /** The Kara lady beetle implementation on top of the grid. */
 class Kara {
-  constructor(grid, coords=Coordinates.fromXy(1,1), direction = Directions.RIGHT) {
+  constructor(grid, coords = Coordinates.fromXy(1, 1), direction = Directions.RIGHT) {
     this.grid = grid;
     this.direction = direction;
     this.coords = Coordinates.fromCopy(coords);
   }
   draw(cell_size) {
     push();
-    translate((this.coords.x+0.5)*cell_size, (this.coords.y+0.5)*cell_size);
+    translate((this.coords.x + 0.5) * cell_size, (this.coords.y + 0.5) * cell_size);
     rotate(this.direction.angle);
     textAlign(CENTER, CENTER);
     text(Cells.BUG.display, 0, 0);
@@ -310,16 +321,16 @@ class Kara {
     return this._cell(this.direction.apply(this.coords)) == Cells.TREE;
   }
   treeLeft() {
-    return this._cell(this.direction.left.apply(this.coords)) == Cells.TREE;    
+    return this._cell(this.direction.left.apply(this.coords)) == Cells.TREE;
   }
-  treeRight() {
-    return this._cell(this.direction.right.apply(this.coords)) == Cells.TREE;    
+  treeRight() {
+    return this._cell(this.direction.right.apply(this.coords)) == Cells.TREE;
   }
   mushroomFront() {
-    return this._cell(this.direction.apply(this.coords)) == Cells.MUSHROOM;    
+    return this._cell(this.direction.apply(this.coords)) == Cells.MUSHROOM;
   }
   onLeaf() {
-    return this._cell(this.coords) == Cells.CLOVER;    
+    return this._cell(this.coords) == Cells.CLOVER;
   }
   _isMoveableDestination(destination) {
     let cell = this._cell(destination);
@@ -334,8 +345,7 @@ class Kara {
   }
   move() {
     if (this._canMove()) {
-      let dest = this.coords.move(this.direction);
-      this.coords = dest;
+      this.coords = this.coords.move(this.direction);
     } else {
       throw new InvalidMoveException(`Unable to move from ${this.coords} in direction ${this.direction}!`);
     }
@@ -361,33 +371,33 @@ class Kara {
 /** A Kara decorator that delays actions (but not sensors) in order to
     allow for stepping replay. */
 class KaraStepper {
-  constructor(kara, delay_ms=500) {
+  constructor(kara, delay_ms = 500) {
     this.kara = kara;
     this.delay = delay_ms;
   }
-  
+
   sleeper() {
     return new Promise(resolve => setTimeout(() => resolve(), this.delay));
   }
-  
+
   treeFront() {
     return this.kara.treeFront();
   }
   treeLeft() {
     return this.kara.treeLeft();
   }
-  treeRight() {
-    return this.kara.treeRight();    
+  treeRight() {
+    return this.kara.treeRight();
   }
   mushroomFront() {
-    return this.kara.mushroomFront();    
+    return this.kara.mushroomFront();
   }
   onLeaf() {
-    return this.kara.onLeaf();    
+    return this.kara.onLeaf();
   }
   async move() {
     await this.sleeper();
-    this.kara.move();  
+    this.kara.move();
   }
   async turnLeft() {
     await this.sleeper();
@@ -424,7 +434,7 @@ class KaraRecorder {
   record(command) {
     // console.log(`recording ${command}`);
     if (this.commands.length > this.maxCommands) {
-      throw new Error(`Stopped recording after ${this.maxCommands} commands.`);
+      throw new TooManyMovesException(`Stopped recording after ${this.maxCommands} commands.`);
     }
     this.commands.push(command);
   }
@@ -435,14 +445,14 @@ class KaraRecorder {
   treeLeft() {
     return this.kara.treeLeft();
   }
-  treeRight() {
-    return this.kara.treeRight();    
+  treeRight() {
+    return this.kara.treeRight();
   }
   mushroomFront() {
-    return this.kara.mushroomFront();    
+    return this.kara.mushroomFront();
   }
   onLeaf() {
-    return this.kara.onLeaf();    
+    return this.kara.onLeaf();
   }
   move() {
     this.record(this.move.name);
@@ -466,10 +476,52 @@ class KaraRecorder {
   }
 }
 
-/** A Kara game including a game grid and one Kara beetle. */
+/** A Kara world including a game grid and one Kara beetle. */
 class KaraWorld {
-  /** Creates a new game from a string spec and sets up a processing setup and draw function. */
-  static create(gridspec=EMPTY_GRID_SPEC, cell_size=25, install=true, keyhandler=true) {
+  /** Creates a new game from a string spec and sets up a processing setup and
+    * draw function.
+    * 
+    * String-specs are processed line-by-line, and each character specifies the 
+    * following cell contents:
+    * <ul>
+    *  <li><code>T</code>: Tree (🌳)
+    *  <li><code>M</code>: Mushroom (🍄)
+    *  <li><code>C</code>: Clover leaf (🍀)
+    *  <li><code>&nbsp;</code> or <code>_</code>: Empty cell
+    *  <li><code><^>v</code>: Kara (🐞) start position and direction.
+    * </ul>
+    * 
+    * <h3>Example</h3>
+    * <code>
+KaraWorld.create(
+    `TTTTTTTTT
+     T       T
+     T  C    T
+     T >   T T
+     T       T
+     M       T
+     T    T  T
+     T       T
+     TTTTTTTTT`)    
+    * </code>
+    *
+    * Lines are trimmed before processing, so they must start and end with
+    * a non-whitespace character (e.g. 'T' or '_').
+    * 
+    * @param {string} [gridspec=EMPTY_GRID_SPEC] the world specification (see   
+    *                 above)
+    * @param {number} [cell_size=25] the size of one cell square in pixels
+    * @param {boolean} [install=true] true to install processing
+    *                  <code>setup</code> and <code>draw</code> functions and a
+    *                  global <code>kara</code> variable.
+    * @param {boolean} [keyhandler=true] true to install a manual movement key 
+    *                  handler for the arrow keys.
+    * @returns {KaraWorld} the created KaraWorld instance
+    */
+  static create(gridspec = EMPTY_GRID_SPEC,
+    cell_size = 25,
+    install = true,
+    keyhandler = true) {
     let game = KaraWorld.fromStringSpec(gridspec);
     if (install) {
       let size = game.grid.gridSize();
@@ -480,6 +532,8 @@ class KaraWorld {
       window.draw = () => {
         game.draw(cell_size);
       };
+      // Set up a global variable for kara for the case that the client
+      // function misses the parameter.
       window.kara = game.getRecorder();
     }
     if (keyhandler) {
@@ -490,13 +544,14 @@ class KaraWorld {
     return game;
   }
 
-  /** Creates a new game from a grid spec. Note that it is not possible to create a game
-   * where Kara's initial square also contains a non-empty cell (leaf or mushroom). */
-  static fromStringSpec(gridspec=EMPTY_GRID_SPEC) {
+  /** Creates a new game from a grid spec. Note that it is not possible to 
+    * create a game where Kara's initial square also contains a non-empty cell 
+    * (leaf or mushroom) using a string spec. */
+  static fromStringSpec(gridspec = EMPTY_GRID_SPEC) {
     let grid = Grid.fromStringSpec(gridspec);
-    let kara = grid.findKara();
-    return new KaraWorld(grid, kara);
+    return new KaraWorld(grid, grid.findKara());
   }
+
   constructor(grid, kara) {
     this.grid = grid;
     this.kara = kara;
@@ -505,27 +560,41 @@ class KaraWorld {
     karaCopy.grid = gridCopy;
     this.recorder = new KaraRecorder(karaCopy);
   }
+
+  /** Draw the grid at (0,0). Use processing transformations to move or
+    * rotate the grid. */
   draw(cell_size) {
     push();
-    textSize(cell_size*0.8);
-    this.grid.draw(cell_size)
+    textSize(cell_size * 0.8);
+    this.grid.draw(cell_size);
     this.kara.draw(cell_size);
-    pop();    
+    pop();
   }
-  
+
+  /** Returns a Kara instance connected to a 'shadow copy' of the game.
+    * Kara commands are executed on the shadow copy can be replayed
+    * on a different Kara instance by calling
+    * <code>recorder.replay(kara)</code>. This is useful to enable
+    * delayed stepping execution of an async Kara implementation such
+    * as <code>KaraStepper</code>. */
   getRecorder() {
     return this.recorder;
   }
-  
+
   /** Executes the well-known client-side 'my_kara(Kara)' function and 
-      record its actions, then replay in async, delayed mode using
-      KaraStepper.
-      <p>This function assumes a deterministic world where each set of
-      commands executed in the same order results in the same outcome. */
-  async executeKara(delay_ms=500, client_function=my_kara) {
-    // TODO check if my_kara is defined.
+    * records its actions, then replays in async, delayed mode using
+    * KaraStepper.
+    * <p>This function assumes a deterministic world where each set of
+    * commands executed in the same order results in the same outcome. */
+  async executeKara(delay_ms = 500, client_function = my_kara, maxCommands = 50) {
+    this.recorder.maxCommands = maxCommands;
     try {
-      client_function(this.recorder); // call well-known function in client-code
+      if (typeof client_function == 'function') {
+         // call well-known function in client-code
+         client_function(this.recorder);
+      } else {
+        throw new Error(`Not a function: ${client_function}`);
+      }
     } catch (e) {
       if (e instanceof InvalidMoveException) {
         // Swallow exception, will raise again during replay
@@ -533,10 +602,10 @@ class KaraWorld {
       // Log other exceptions (e.g. too many moves), but do not abort.
       console.log(e);
     }
-    let stepper = new KaraStepper(this.kara, delay_ms); 
+    let stepper = new KaraStepper(this.kara, delay_ms);
     return this.recorder.replay(stepper);
   }
-  
+
   /** A key-handler for manual Kara movement. */
   keyPressed() {
     if (keyCode == UP_ARROW) {
@@ -556,6 +625,6 @@ class KaraWorld {
       }
       return false;
     }
-  }  
+  }
 }
 
